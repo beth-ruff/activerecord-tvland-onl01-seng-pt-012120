73cb557af05cd5885561e90a0f983578a7ba16e5
Beth Ruff <beth.ruff2@gmail.com>
2020-04-08 02:44:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   
   def actors_list
     actors_list = []
-    array_of_actors = self.characters.actor {|actor| actor.full_name}
+    array_of_actors = self.actors {|actors| actors.full_name}
     actors_list << array_of_actors
     actors_list
   end 
